d221fd444d717b87f4011b679358b771ab67a807
Nil Portugués Calderó <nilopc@gmail.com>
2014-02-03 15:05:52 +0100

    Moved tests directory

--- tests/Sonrisa/Service/ShortLink/GoogleTest.php
+++ tests/Sonrisa/Service/ShortLink/GoogleTest.php
@@ -1,6 +1,10 @@
 <?php
-
-namespace Sonrisa\Service\ShortLink\Tests;
+/*
+ * Author: Nil Portugués Calderó <contact@nilportugues.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 class GoogleTest extends \PHPUnit_Framework_TestCase
 {
